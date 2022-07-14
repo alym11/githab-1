2f11ec353833de3b20ef8061ba499fc5401d6efa
alym11 <109304650+alym11@users.noreply.github.com>
2022-07-15 00:49:40 +0600

    Add files via upload

---
+++ dart_application_1.dart
@@ -0,0 +1,22 @@
+
+
+
+void main(List<String> arguments) {
+   
+double b = 4.20;
+int a = 3;
+ print (a + b);
+
+String text = 'Name';
+print(text);
+text = 'Flutter';
+print(text);
+
+
+
+
+§
+
+
+
+}
